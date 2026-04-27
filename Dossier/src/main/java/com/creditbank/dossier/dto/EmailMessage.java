013546948da83715af20fa0b4e1d57964b11dc35
x-t4m-cx <t4bakovm@yandex.ru>
2026-04-27 10:06:09 +0400

    configure SMTP and add sending email

--- Dossier/src/main/java/com/creditbank/dossier/dto/EmailMessage.java
+++ Dossier/src/main/java/com/creditbank/dossier/dto/EmailMessage.java
@@ -4,10 +4,12 @@ import com.creditbank.dossier.enums.EmailTheme;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.UUID;
 
 @Getter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmailMessage {
--- Dossier/src/main/java/com/creditbank/dossier/service/EmailService.java
+++ Dossier/src/main/java/com/creditbank/dossier/service/EmailService.java
@@ -1,14 +1,26 @@
 package com.creditbank.dossier.service;
 
 import com.creditbank.dossier.dto.EmailMessage;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class EmailService {
 
+    public final JavaMailSender emailSender;
+
     public void sendEmail(EmailMessage message){
-        log.info("message received:{}", message);
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        simpleMailMessage.setTo(message.getAddress());
+        simpleMailMessage.setSubject(message.getTheme().toString());
+        simpleMailMessage.setText(message.getText());
+
+        emailSender.send(simpleMailMessage);
     }
 }
